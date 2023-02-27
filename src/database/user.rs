4442e4a1c612f3b302ef436d8a9e09518cf6c46f
Paul-Christian Volkmer <volkmer_p@ukw.de>
2023-02-27 11:25:02 +0100

    Fix misspelled database column

--- src/database/user.rs
+++ src/database/user.rs
@@ -4,7 +4,7 @@ use mysql::prelude::Queryable;
 
 pub fn update_password(db: &Database, login: &Option<String>, new_password: &String) {
     if let Some(login) = login {
-        let sql = "UPDATE akteur SET password = SHA2(:new_password, 256) \
+        let sql = "UPDATE akteur SET passwort = SHA2(:new_password, 256) \
                 WHERE login = :login";
         let _ = db.connection().exec_drop(
             sql,
@@ -12,7 +12,7 @@ pub fn update_password(db: &Database, login: &Option<String>, new_password: &Str
         );
     }
 
-    let sql = "UPDATE akteur SET password = SHA2(:new_password, 256)";
+    let sql = "UPDATE akteur SET passwort = SHA2(:new_password, 256)";
     let _ = db
         .connection()
         .exec_drop(sql, params! {"new_password" => new_password});
